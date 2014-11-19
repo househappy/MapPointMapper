fbf0e3d986d7b3ec36e40d67921c6db833711cf8
dmiedema <daniel@househappy.org>
2014-11-18 16:33:46 -0800

    Initial Commit

---
+++ MapPointMapper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MapPointMapper
+//
+//  Created by Daniel on 11/18/14.
+//  Copyright (c) 2014 dmiedema. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
