e2f59d3a87010cb177332033c19281312f3bf8fd
dmiedema <daniel@househappy.org>
2015-02-16 10:39:39 -0800

    Documentation

--- MapPointMapper/Parser/Parser.swift
+++ MapPointMapper/Parser/Parser.swift
@@ -10,14 +10,18 @@ import Foundation
 import MapKit
 
 extension NSString {
-    /*!
+    /**
+    Convienence so `isEmpty` can be performed on an `NSString` instance just as if it were a `String`
     */
     var isEmpty: Bool {
         get { return self.length == 0 || self.isEqualToString("") }
     }
 }
 extension String {
-    /*!
+    /**
+    Strip all leading and trailing whitespace from a given `String` instance.
+    
+    :returns: a newly stripped string instance.
     */
     func stringByStrippingLeadingAndTrailingWhiteSpace() -> String {
         let mutable = self.mutableCopy() as NSMutableString
@@ -28,6 +32,16 @@ extension String {
 
 class Parser {
     // MARK: - Public
+    /**
+    Parse a given string of Lat/Lng values to return a collection of `CLLocationCoordinate2D` arrays.
+
+    :note: The preferred way/format of the input string is `Well-Known Text` as the parser supports that for multipolygons and such
+
+    :param: input          String to parse
+    :param: longitudeFirst Only used if it is determined to not be `Well-Known Text` format.
+
+    :returns: An array of `CLLocationCoordinate2D` arrays representing the parsed areas/lines
+    */
     class func parseString(input: NSString, longitudeFirst: Bool) -> [[CLLocationCoordinate2D]] {
         return Parser(longitudeFirst: longitudeFirst).parseInput(input)
     }
@@ -42,6 +56,16 @@ class Parser {
     // MARK: - Private
     
     // MARK: Parsing
+
+    /**
+    Parse input string into a collection of `CLLocationCoordinate2D` arrays that can be drawn on a map
+
+    :note: This method supports (and really works best with/prefers) `Well-Known Text` format
+
+    :param: input `NSString` to parse
+
+    :returns: Collection of `CLLocationCoordinate2D` arrays
+    */
     internal func parseInput(input: NSString) ->  [[CLLocationCoordinate2D]] {
         var array = [[NSString]]()
         
@@ -65,7 +89,16 @@ class Parser {
         
         return results.filter({ !$0.isEmpty }).map{ self.convertToCoordinates($0, longitudeFirst: self.longitudeFirst) }
     }
+
+    /**
+    Convert an array of strings into tuple pairs.
     
+    :note: the number of values passed in should probaly be even, since it creates pairs.
+
+    :param: array of `[NSString]` array to create tuples from
+
+    :returns: array of collections of tuple pairs where the tuples are lat/lng values as `NSString`s
+    */
     internal func convertStringArraysToTuples(array: [[NSString]]) -> [[(NSString, NSString)]] {
         var tmpResults = [(NSString, NSString)]()
         var results = [[(NSString, NSString)]]()
@@ -83,7 +116,16 @@ class Parser {
         } // end for arr in array
         return results
     }
+
+    /**
+    :abstract: Naively format a `Well-Known Text` string into array of string values, where each string is a single value
     
+    :disucssion: This removes any lingering parens from the given string, breaks on `,` then breaks on ` ` while filtering out any empty strings.
+
+    :param: input String to format, assumed `Well-Known Text` format
+
+    :returns: array of strings where each string is one value from the string with all empty strings filtered out.
+    */
     internal func formatStandardGeoDataString(input: NSString) -> [NSString] {
         // Remove Extra ()
         let stripped = input
@@ -113,7 +155,14 @@ class Parser {
     }
     
     /**
-    Convert [(String, String)] array of tuples into a [CLLocationCoordinate2D]
+    :abstract: Convert a given array of `(String, String)` tuples to array of `CLLocationCoordinate2D` values
+    
+    :discussion: This attempts to parse the strings double values but does no safety checks if they can be parsed as `double`s.
+
+    :param: pairs          array of `String` tuples to parse as `Double`s
+    :param: longitudeFirst boolean flag if the first item in the tuple should be the longitude value
+
+    :returns: array of `CLLocationCoordinate2D` values
     */
     internal func convertToCoordinates(pairs: [(NSString, NSString)], longitudeFirst: Bool) -> [CLLocationCoordinate2D] {
         var coordinates = [CLLocationCoordinate2D]()
@@ -131,16 +180,20 @@ class Parser {
         }
         return coordinates
     }
-    
+
     /**
     Removes any text before lat long points as well as two outer sets of parens.
     
     Example:
     input  => "POLYGON(( 15 32 ))"
     output => "15 32"
-    
+
     input  => "MULTIPOLYGON((( 15 32 )))"
     output => "( 15 32 )"
+
+    :param: input NSString to strip extraneous characters from
+
+    :returns: stripped string instance
     */
     internal func stripExtraneousCharacters(input: NSString) -> NSString {
         let regex = NSRegularExpression(pattern: "\\w+\\s+\\((.*)\\)", options: .CaseInsensitive, error: nil)
@@ -150,9 +203,18 @@ class Parser {
         let loc = range?.location as Int!
         let len = range?.length as Int!
         
-        return (input as NSString).substringWithRange(NSRange(location: loc, length: len)) as NSString
+        return input.substringWithRange(NSRange(location: loc, length: len)) as NSString
     }
-    
+
+    /**
+    :abstract: Attempt to determine if a given string is in `Well-Known Text` format (GeoString as its referred to internally)
+
+    :discussion: This strips any leading & trailing white space before checking for the existance of word characters at the start of the string.
+
+    :param: input String to attempt determine if is in `Well-Known Text` format
+
+    :returns: `true` if it thinks it is, `false` otherwise
+    */
     internal func isProbablyGeoString(input: String) -> Bool {
         let stripped = input.stringByStrippingLeadingAndTrailingWhiteSpace()
         if let geoString = stripped.rangeOfString("^\\w+", options: .RegularExpressionSearch) {
@@ -160,7 +222,14 @@ class Parser {
         }
         return false
     }
-    
+
+    /**
+    Determine if a given string is a `MULTI*` item.
+
+    :param: input String to check
+
+    :returns: `true` if the string starts with `MULTI`. `false` otherwise
+    */
     internal func isMultiItem(input: String) -> Bool {
         if let isPolygon = input.rangeOfString("MULTI", options: .RegularExpressionSearch) {
             return true
